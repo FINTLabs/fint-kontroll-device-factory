26cc6f74bcceff161ea992d7e28d5ba2e590de43
chadaj <agata.chadaj@novari.no>
2025-11-14 14:30:03 +0100

    json ignore

--- src/main/kotlin/no/novari/fintkontrolldevicefactory/entity/DeviceGroupMembership.kt
+++ src/main/kotlin/no/novari/fintkontrolldevicefactory/entity/DeviceGroupMembership.kt
@@ -1,8 +1,11 @@
 package no.novari.fintkontrolldevicefactory.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 data class DeviceGroupMembership (
     val groupId: String,
     val deviceId: String
 ){
+    @JsonIgnore
     fun getId(): String = "${deviceId}_${groupId}"
 }
